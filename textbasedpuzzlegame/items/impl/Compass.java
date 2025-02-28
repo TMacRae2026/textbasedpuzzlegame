b9533bfb242e59b83f69c8110a582dcedcbf68ec
TMacRae2026 <tmacrae2026@cchsdons.com>
2025-02-27 22:10:12 -0800

    Add files via upload

---
+++ textbasedpuzzlegame/items/impl/Compass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.textbasedpuzzlegame.items.impl;
+
+import com.mycompany.textbasedpuzzlegame.items.Item;
+
+/**
+ *
+ * @author TMacRae2026
+ */
+public class Compass extends Item {
+    
+    public Compass() {
+        super("Compass");
+    }
+    //This item is a passive item and doesn't require a use command
+    
+}
